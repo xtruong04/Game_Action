f1570ff5cb19732056f84369a1ff0dda13c48fd5
vinhvinh450 <vinhvinh450@gmail.com>
2026-01-06 14:50:07 +0700

    Tuần 4

--- Assets/Srcipts/Portal.cs
+++ Assets/Srcipts/Portal.cs
@@ -4,12 +4,27 @@ using UnityEngine.SceneManagement;
 public class Portal : MonoBehaviour
 {
     [SerializeField] private string nextSceneName;
+    private float startTime;
+
+    void Start()
+    {
+        // L?u th?i gian b?t ??u màn
+        startTime = Time.time;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene("LevelSelectScene");
+            float finishTime = Time.time;
+            float duration = finishTime - startTime;
+
+            // L?u th?i gian hoàn thành màn hi?n t?i
+            string currentLevel = SceneManager.GetActiveScene().name;
+            PlayerPrefs.SetFloat(currentLevel + "_CompletionTime", duration);
+
+            // Load sang màn ch?n level
+            SceneManager.LoadScene(nextSceneName);
         }
     }
 }
